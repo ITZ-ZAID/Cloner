64348b4c66fc804e41aab0b315f29636b0fe129a
Zaid <itsunknown122@gmail.com>
2022-06-27 00:21:39 +0530

    Published

--- zaid.py
+++ zaid.py
@@ -19,7 +19,9 @@ ZAID = Client("ZPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 async def hello(client: ZAID, message: Message):
     await message.reply("Hey! It's Just a Cloner Bot example source Code")
 
+##Copy from here 
 
+© By Itz-Zaid Your motherfucker if uh Don't gives credits.
 @ZAID.on_message(filters.private & filters.command("clone"))
 async def clone(bot: ZAID, msg: Message):
     chat = msg.chat
@@ -28,6 +30,7 @@ async def clone(bot: ZAID, msg: Message):
     phone = msg.command[1]
     try:
         await text.edit("Booting Your Client")
+                   # change this Directry according to ur repo
         client = Client(":memory:", API_ID, API_HASH, bot_token=phone, plugins={"root": "handlers"})
         await client.start()
         idle()
@@ -35,6 +38,9 @@ async def clone(bot: ZAID, msg: Message):
         await msg.reply(f"Your Client Has Been Successfully Started As @{user.username}! ✅ \n\n Now Add Your Bot And Assistant @Amala_music_assistant_1 To Your Chat!\n\nThanks for Cloning.")
     except Exception as e:
         await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
+#End
+##This code fit with every pyrogram Codes just import then @Client Xyz!
+
 
 ZAID.start()
 idle()
