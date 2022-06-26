d5745bf3773a003e3281d6a47d9a4c846f98334b
Zaid <itsunknown122@gmail.com>
2022-06-27 00:17:37 +0530

    Published Base things

---
+++ handlers/start.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from pyrogram.types import *
+from pyrogram import filters
+
+
+
+@Client.on_message(filters.private & filters.command("start"))
+async def start(client: Client, message: Message):
+    await message.reply("Hey! It's Just a Cloner Bot example source Code")
+
